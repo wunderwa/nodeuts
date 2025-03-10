676b97e361f2ff2b9b095d0192419ab332b679c4
Andrei Zagoruiko <adkrix@gmail.com>
2025-03-10 15:43:47 +0300

    execLiveLog fix

--- src/process.ts
+++ src/process.ts
@@ -3,8 +3,8 @@ import child_process from 'node:child_process'
 export type ExecLog = 'all' | 'data' | 'error' | 'none'
 
 export type ExecOptions = {
-  cwd: string
-  log: ExecLog
+  cwd?: string
+  log?: ExecLog
 }
 
 export const execLiveLog = async (
@@ -17,10 +17,10 @@ export const execLiveLog = async (
 
     command.stdout?.on('data', (output) => {
       response.push(output)
-      if (['data', 'all'].includes(log)) console.log(output.toString())
+      if (['data', 'all'].includes(log ?? '')) console.log(output.toString())
     })
     command.stdout?.on('error', (output) => {
-      if (['error', 'all'].includes(log)) console.log(output.toString())
+      if (['error', 'all'].includes(log ?? '')) console.log(output.toString())
       reject(output.toString())
     })
 
